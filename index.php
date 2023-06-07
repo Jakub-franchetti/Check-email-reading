76cfce9a4021ed541802f27833e76b81157c0109
Jakub-franchetti <homoncik.jakubpawel@franchettisalviani.net>
2023-06-07 11:17:18 +0200

    versione 1.1

--- index.php
+++ index.php
@@ -1,46 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Email Form</title>
+</head>
+<body>
+    <form method="post" >
+        <label for="email">Recipient Email:</label>
+        <input type="email" id="email" name="email" required value="@gmail.com">
+        <button type="submit" name="send">Send Email</button>
+    </form>
+</body>
+</html>
 <?php
-// Parametri email
-$destinatario = 'recipient email';
-$oggetto = 'Oggetto dell\'email';
-$messaggio = 'Corpo dell\'email';
+if(isset($_POST['send'])){
+    // Parametri email
+    $destinatario = $_POST['email'];
+    $oggetto = 'Oggetto dell\'email';
+    $messaggio = 'Corpo dell\'email';
 
-$codiceMonitoraggio = uniqid();
+    $codiceMonitoraggio = uniqid();
 
-// URL dell'immagine di monitoraggio
-$urlMonitoraggio = 'https://www.wosknet.com/SitoInnocuo/sus2/track.php?id=' . $codiceMonitoraggio;
+    // URL dell'immagine di monitoraggio
+    $urlMonitoraggio = 'https://www.wosknet.com/SitoInnocuo/sus2/track.php?id=' . $codiceMonitoraggio;
 
-// Codice HTML dell'immagine di monitoraggio
-$immagineMonitoraggio = '<img src="' . $urlMonitoraggio . '" alt="" />';
+    // Codice HTML dell'immagine di monitoraggio
+    $immagineMonitoraggio = '<img src="' . $urlMonitoraggio . '" alt="" />';
 
-// Aggiungi l'immagine di monitoraggio al corpo dell'email
-$messaggio .= $immagineMonitoraggio;
+    // Aggiungi l'immagine di monitoraggio al corpo dell'email
+    $messaggio .= $immagineMonitoraggio;
 
-// Intestazioni dell'email
-//$message = "Questa mail è stata inviata perchè è stata visualizzata l'immagine all'indirizzo: <a href='https://www.ricecipriani.it/img.svg.php'>smile.svg</a>";
+    // Intestazioni dell'email
+    $headers = "From: noreply@gmail.com\r\n";
+    $headers .= "Reply-To: noreply@gmail.com\r\n";
+    $headers .= "Cc: noreply@gmail.com\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=utf-8\r\n";
 
- $headers = "From: noreply@gmail.com\r\n";
- $headers = "Reply-To: noreply@gmail.com\r\n";
- $headers .= "Cc: noreply@gmail.com\r\n";
- $headers = "MIME-Version: 1.0\r\n";
- $headers = "Content-Type: text/html; charset=utf-8\r\n";
+    // Salva il codice di monitoraggio e la mail su ore.txt
+    salvaCodiceEmail($codiceMonitoraggio, $destinatario);
 
- // Invio dell'email utilizzando la funzione mail() di PHP
- $mailSent = mail($to, $subject, $message, $headers);
-
-// Invia l'email
-if (mail($destinatario, $oggetto, $messaggio, $headers)) {
-    // Salvataggio del codice di monitoraggio su un file di testo
-    salvaCodiceMonitoraggio($codiceMonitoraggio);
-    echo 'Email inviata con successo.';
-} else {
-    echo 'Si è verificato un errore durante l\'invio dell\'email.';
+    // Invia l'email
+    if (mail($destinatario, $oggetto, $messaggio, $headers)) {
+        echo 'Email inviata con successo a '.$destinatario;
+    } else {
+        echo 'Si è verificato un errore durante l\'invio dell\'email.';
+    }
 }
 
-function salvaCodiceMonitoraggio($codiceMonitoraggio) {
-    // Percorso del file di testo per il salvataggio del codice di monitoraggio
+function salvaCodiceEmail($codiceMonitoraggio, $email) {
+    // Percorso del file di testo per il salvataggio del codice di monitoraggio e dell'email
     $percorsoFile = 'ore.txt';
-    
-    // Aggiunta del codice di monitoraggio al file di testo
-    file_put_contents($percorsoFile, $codiceMonitoraggio . PHP_EOL, FILE_APPEND);
+
+    // Creazione della riga da salvare sul file
+    $riga = $codiceMonitoraggio . '|' . $email . PHP_EOL;
+
+    // Aggiunta della riga al file di testo
+    file_put_contents($percorsoFile, $riga, FILE_APPEND);
 }
 ?>
--- track.php
+++ track.php
@@ -11,12 +11,55 @@ function salvaLetturaMonitoraggio($codiceMonitoraggio) {
 
     // Aggiunta della riga al file di testo
     file_put_contents($percorsoFile, $riga, FILE_APPEND);
+
+    // Invia una seconda email di conferma
+    inviaEmailConferma($codiceMonitoraggio);
+}
+
+function inviaEmailConferma($codiceMonitoraggio) {
+    // Percorso del file di testo per il recupero dell'email corrispondente al codice di monitoraggio
+    $percorsoFile = 'ore.txt';
+
+    // Legge il file di testo
+    $contenuto = file_get_contents($percorsoFile);
+
+    // Cerca il codice di monitoraggio e l'email corrispondente
+    $righe = explode(PHP_EOL, $contenuto);
+    foreach ($righe as $riga) {
+        $rigaSeparata = explode('|', $riga);
+        if ($rigaSeparata[0] == $codiceMonitoraggio) {
+            $destinatario = $rigaSeparata[1];
+            break;
+        }
+    }
+
+    // Verifica se è stata trovata una corrispondenza
+    if (isset($destinatario)) {
+        // Parametri email
+        $oggetto = 'Grazie per aver aperto la mail precedente!';
+        $messaggio = 'Gentile destinatario,
+
+    Grazie per aver aperto la mail precedente.
+
+    Cordiali saluti,
+    Il tuo nome';
+
+        // Intestazioni dell'email
+        $headers = "From: noreply@example.com\r\n";
+        $headers .= "Reply-To: noreply@example.com\r\n";
+        $headers .= "Cc: noreply@example.com\r\n";
+        $headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-Type: text/plain; charset=utf-8\r\n";
+
+        // Invio dell'email utilizzando la funzione mail() di PHP
+        mail($destinatario, $oggetto, $messaggio, $headers);
+    }
 }
 
 // Ottieni l'ID di monitoraggio dall'URL
 $codiceMonitoraggio = $_GET['id'];
 
-// Salvataggio dell'ora di apertura dell'email sul file di testo
+// Salvataggio dell'ora di apertura dell'email sul file di testo e invio dell'email di conferma
 salvaLetturaMonitoraggio($codiceMonitoraggio);
 
 // Ritorna l'immagine nove.jpg
